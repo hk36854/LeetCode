4c7a3152f0afc40fbf09091b5be57a8551735971
hk36854 <37497215+hk36854@users.noreply.github.com>
2023-07-01 23:36:06 +0900

    20. Valid Parentheses

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,31 @@
+# @before-stub-for-debug-begin
+from python3problem20 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
+
+
+
+# @lc code=end
+
